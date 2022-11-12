ebb158b1453382281a08e1a5754b99ac3b688d36
Lzyct <hey.mudassir@gmail.com>
2022-11-13 01:26:39 +0800

    (clean): remove history folder

--- .history/data/headerNavLinks_20221112215836.js
+++
@@ -1,7 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: 'https://lazycatlabs.com/', title: 'About' },
-]
-
-export default headerNavLinks
