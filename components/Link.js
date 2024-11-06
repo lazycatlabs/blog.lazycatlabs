351962b3f529d749fd5a33ad16a3ee7f7613da77
Lzyct <hey.mudassir@gmail.com>
2024-11-06 14:49:58 +0800

    fix: Link issue after update to NextJS 15

--- components/Link.js
+++ components/Link.js
@@ -1,22 +1,22 @@
 import Link from 'next/link'
 
-const CustomLink = ({ href, className, ...rest }) => {
+const CustomLink = ({ href, className, target, ...rest }) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
     return (
-      <Link key={href} href={href} className={className}>
+      <Link key={href} href={href} target={target} className={className} passHref legacyBehavior>
         <span {...rest} />
       </Link>
     )
   }
 
   if (isAnchorLink) {
-    return <Link href={href} {...rest} />
+    return <a key={href} target={target} href={href} className={className} {...rest} />
   }
 
-  return <Link target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+  return <a key={href} target="_blank" rel="noopener noreferrer" href={href} {...rest} />
 }
 
 export default CustomLink
