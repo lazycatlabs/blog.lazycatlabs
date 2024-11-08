f3d30ab95be928c0996b68904621ed5f49d24e96
Lzyct <hey.mudassir@gmail.com>
2024-11-08 18:46:49 +0800

    feat: update 404 page

--- pages/404.js
+++ pages/404.js
@@ -8,7 +8,7 @@ export default function FourZeroFour() {
       <PageSEO title={`Page Not Found - ${siteMetadata.title}`} description="" />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-          <h1 className=" font-extra bold md:leading-14 text-6xl leading-9 tracking-tight text-text md:border-r-2 md:px-6 md:text-8xl">
+          <h1 className="animate-text bg-gradient-to-r from-teal via-pink to-peach bg-clip-text px-4 py-8 text-8xl font-black text-transparent md:border-r-2">
             404
           </h1>
         </div>
@@ -20,7 +20,7 @@ export default function FourZeroFour() {
             But dont worry, you can find plenty of other things on our homepage.
           </p>
           <CustomLink href="/">
-            <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-peach px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-peach focus:outline-none">
+            <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-peach px-4 py-2 text-base text-sm font-medium leading-5 shadow transition-colors duration-150  focus:outline-none">
               Back to homepage
             </button>
           </CustomLink>
