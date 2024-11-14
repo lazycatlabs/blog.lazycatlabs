4c07111bfaf824e4dee919114d4ba769db64eac2
Munir AP <softwaremakassar@gmail.com>
2024-11-14 22:01:47 +0800

    feat: add github actions (#29)
    * feat: add github actions
    
    * chore: add workflow dispatch trigger
    
    * chore: uncomment docker build line

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enable this only on the local machine not in the VPS
-#docker build  --no-cache -t lazycatlabs/blog . --platform linux/amd64
+docker build  --no-cache -t lazycatlabs/blog . --platform linux/amd64
 
 # Bring up the Docker containers using docker-compose
 docker compose -f docker-compose.yml up -d
