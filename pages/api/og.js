4d08bf2257e3cebe931d313124382245ce08c941
Lzyct <hey.mudassir@gmail.com>
2024-11-15 22:16:32 +0800

    fix: og issue

--- pages/api/og.js
+++ pages/api/og.js
@@ -90,6 +90,9 @@ export default async function handler(req) {
     {
       width: 1200,
       height: 630,
+      headers: {
+        'Content-Type': 'image/png',
+      },
       fonts: fontData
         ? [
             {
