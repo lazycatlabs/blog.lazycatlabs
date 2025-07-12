495be28ac799740f2550f2c812d8fe3537049249
Lzyct <hey.mudassir@gmail.com>
2025-07-12 23:33:00 +0800

    fix: content security

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com  *.google-analytics.com *.googlesyndication.com *.doubleclick.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com  *.google-analytics.com *.googlesyndication.com *.doubleclick.net *.adtrafficquality.google;
   style-src 'self' 'unsafe-inline' giscus.app googlesyndication.com doubleclick.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app youtube.com googlesyndication.com doubleclick.net www.youtube.com;
+  frame-src giscus.app youtube.com googlesyndication.com googleads.g.doubleclick.net doubleclick.net www.youtube.com;
 `
 
 const securityHeaders = [
