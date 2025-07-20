27c98235ea084f2d8de87737b3d943ddea7d8b36
Lzyct <hey.mudassir@gmail.com>
2025-07-20 20:36:01 +0800

    fix: og issues

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,35 +1,19 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import LogoLight from '@/data/logo_light.svg'
-import LogoDark from '@/data/logo_dark.svg'
 import CustomLink from '@/components/Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import { useEffect, useState } from 'react'
-import { useTheme } from 'next-themes'
 
 const LayoutWrapper = ({ children }) => {
-  const [isMounted, setMounted] = useState(false)
-  const { theme } = useTheme()
-
-  // When mounted on client, now we can show the UI
-  useEffect(() => {
-    document.body.classList = theme
-    setMounted(true)
-  }, [theme])
-
-  if (!isMounted) {
-    return null
-  }
-
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
         <header className="flex items-center justify-between py-6">
           <CustomLink href="/" aria-label={siteMetadata.headerTitle}>
-            {isMounted && theme === 'black' ? <LogoLight /> : <LogoDark />}
+            <LogoLight />
           </CustomLink>
           <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
